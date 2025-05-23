d8740946200abfaa17ed22ece4cfedbb0678b426
kmk0723 <manikarthikeya1@gmail.com>
2025-05-23 12:48:34 +0530

    Added Base Project Files

---
+++ src/test/java/com/example/empay/EmpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
